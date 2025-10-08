2226d47157ce55003dd4d5302f8fdeaf3b6cc5cf
Francydio <57456069+Francydio@users.noreply.github.com>
2025-10-08 17:29:29 +0200

    Adaptación de tests para clase y actualización de versiones (#1)

--- src/main/java/es/upm/grise/profundizacion/fibonacci/Fibonacci.java
+++ src/main/java/es/upm/grise/profundizacion/fibonacci/Fibonacci.java
@@ -17,36 +17,37 @@ public class Fibonacci {
 			
 		if (n <= 1)
 			return n;
-						
+		
 		return(calculate(n-1) + calculate(n-2));
-		}
+	}
 
 	/**
 	 * Function that calculates Fibonacci's number (iterative)
 	 * @param n
 	 * @return		Fibonacci's number
 	 * @throws InvalidNumberException */
-	 
-/*
-   	public static long calculate(int n) throws InvalidNumberException {
-		long fn_minus_2 = 0;
-		long fn_minus_1 = 1;
-		long fn = 2; // Unnecessary
-		
-		if ((n > 92) || (n < 0))
-			throw new InvalidNumberException();
-		
-		if (n <= 1)
-			return n;
-		
-		for (int i=2; i <= n; i++) {
-			fn = fn_minus_1 + fn_minus_2;
-			fn_minus_2 = fn_minus_1;
-			fn_minus_1 = fn;
-		}
-			
-		return fn;
-	}
-*/
+	
+//	public static String TYPE = "Iterative";
+//
+//   	public static long calculate(int n) throws InvalidNumberException {
+//		long fn_minus_2 = 0;
+//		long fn_minus_1 = 1;
+//		long fn = 2; // Unnecessary
+//		
+//		if ((n > 92) || (n < 0))
+//			throw new InvalidNumberException();
+//		
+//		if (n <= 1)
+//			return n;
+//		
+//		for (int i=2; i <= n; i++) {
+//			fn = fn_minus_1 + fn_minus_2;
+//			fn_minus_2 = fn_minus_1;
+//			fn_minus_1 = fn;
+//		}
+//			
+//		return fn;
+//	}
+
 	
 }
--- src/test/java/es/upm/grise/profundizacion/fibonacci/FibonacciTest.java
+++ src/test/java/es/upm/grise/profundizacion/fibonacci/FibonacciTest.java
@@ -1,5 +1,7 @@
 package es.upm.grise.profundizacion.fibonacci;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,34 +11,33 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import static org.junit.jupiter.api.Timeout.ThreadMode.SEPARATE_THREAD;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assumptions.assumingThat;
+
 
 import java.util.concurrent.TimeUnit;
 
 public class FibonacciTest {
-	
-	@BeforeAll
-	static public void beforeAll() {
-		System.out.println("Executed before any test has been run");
-	}
-	
-	@AfterAll
-	static public void afterAll() {
-		System.out.println("Executed after all tests have been run");
-	}
-	
-	@BeforeEach
-	public void beforeEach() {
-		System.out.println("Executed before each test");
-	}
-	
-	@AfterEach
-	public void afterEach() {
-		System.out.println("Executed after each test");
-	}
 
+//	@BeforeAll
+//	static public void beforeAll() {
+//		System.out.println("Executed before any test has been run");
+//	}
+//	
+//	@AfterAll
+//	static public void afterAll() {
+//		System.out.println("Executed after all tests have been run");
+//	}
+//	
+//	@BeforeEach
+//	public void beforeEach() {
+//		System.out.println("Executed before each test");
+//	}
+//	
+//	@AfterEach
+//	public void afterEach() {
+//		System.out.println("Executed after each test");
+//	}
+
+	
 	@DisplayName("The fibonacci of 0 is 0")
 	@Test()
 	public void value_0_returns_0() throws InvalidNumberException {
@@ -54,17 +55,17 @@ public class FibonacciTest {
 	}
 	
 	@Test
-	@Timeout(value = 4, unit = TimeUnit.SECONDS, threadMode = SEPARATE_THREAD)
+	//@Timeout(value = 4, unit = TimeUnit.SECONDS, threadMode = SEPARATE_THREAD)
 	public void value_47_returns_2971215073() throws InvalidNumberException {
 		assertEquals(2971215073L, Fibonacci.calculate(47));
 	}
 	
-	@Test
-	public void value_92_returns_7540113804746346429() throws InvalidNumberException {
-		assumingThat(
-				Fibonacci.TYPE.equals("Iterative"),
-				() -> assertEquals(7540113804746346429L, Fibonacci.calculate(92)));
-	}
+//	@Test
+//	public void value_92_returns_7540113804746346429() throws InvalidNumberException {
+//		assumingThat(
+//				Fibonacci.TYPE.equals("Iterative"),
+//				() -> assertEquals(7540113804746346429L, Fibonacci.calculate(92)));
+//	}
 	
 	@Disabled
 	@Test
@@ -72,8 +73,8 @@ public class FibonacciTest {
 		assertThrows(InvalidNumberException.class, () -> Fibonacci.calculate(93));
 	}
 	
-	@Test
-	public void value_Minus_1_Throws_Exception() throws InvalidNumberException {
-		assertThrows(InvalidNumberException.class, () -> Fibonacci.calculate(-1));
-	}
+//	@Test
+//	public void value_Minus_1_Throws_Exception() throws InvalidNumberException {
+//		assertThrows(InvalidNumberException.class, () -> Fibonacci.calculate(-1));
+//	}
 }
